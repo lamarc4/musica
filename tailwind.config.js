6486cd2fcaa4eb133fe681dd238821f1f5910220
lamarc4 <g.lamarca@outlook.com>
2024-08-05 10:07:23 -0300

    aside

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        ligth: '#EFEEE0',
+        dark: '#1D2123',
+        secondary: '#FACD66',
+        primary: '#307DB8',
+        alt: '#A4C7C6',
+        dark_alt: '#1A1E1F',
+      },
+    },
+  },
+  plugins: [],
+}
+
